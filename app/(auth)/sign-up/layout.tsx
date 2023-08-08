b006033543f25e86a7d22b6e421f7284c342c331
valensixx <valensixx@gmail.com>
2023-08-08 18:27:25 +0300

    working on layout and seo optimization

--- app/(auth)/sign-up/layout.tsx
+++ app/(auth)/sign-up/layout.tsx
@@ -1,7 +1,20 @@
 //This is the SEO (SEARCH ENGINE OPTIMIZATION):
 
+import { ClerkProvider } from "@clerk/nextjs"
+
 export const metadata = {
     title: 'Threadz',
-    description: 'Threadz, Next13 MERN applicaton with React, Next JS, TypeScript and MongoDB.', 
+    description: 'Threadz, Next13 MERN applicaton with React, Next JS, TypeScript and MongoDB.',
 }
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className="">
+
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
