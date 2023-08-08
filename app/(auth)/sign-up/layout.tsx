b95ec5f538f0defd86d414a2875a6c160d2fe233
valensixx <valensixx@gmail.com>
2023-08-08 18:38:15 +0300

    layout done for now

--- app/(auth)/sign-up/layout.tsx
+++ app/(auth)/sign-up/layout.tsx
@@ -1,20 +1,24 @@
 //This is the SEO (SEARCH ENGINE OPTIMIZATION):
 
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
 
 export const metadata = {
     title: 'Threadz',
     description: 'Threadz, Next13 MERN applicaton with React, Next JS, TypeScript and MongoDB.',
 }
 
+const inter = Inter({ subsets: ['latin'] }); //Impoet the font we are using in the project!
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className="">
-
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
                 </body>
             </html>
         </ClerkProvider>
-    )
+    );
 }
