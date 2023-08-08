c582c18cc5b04b0c7ae41517539036bcb3390bf3
valensixx <valensixx@gmail.com>
2023-08-08 18:12:49 +0300

    setting up the file and folder sructure

---
+++ app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,7 @@
+//This is the SEO (SEARCH ENGINE OPTIMIZATION):
+
+export const metadata = {
+    title: 'Threadz',
+    description: 'Threadz, Next13 MERN applicaton with React, Next JS, TypeScript and MongoDB.', 
+}
+
