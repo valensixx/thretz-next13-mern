a5c1d243f9a5637439a73efdeb9033a48fc0c4e3
valensixx <valensixx@gmail.com>
2023-08-09 15:46:06 +0300

    localhost display errors fixed,install dark theme

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from 'react';
+import '../globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,11 +15,17 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
