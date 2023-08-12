ad75e1ca3974eff1067e38a8ec1b8479276da245
valensixx <valensixx@gmail.com>
2023-08-12 13:50:06 +0300

    isActive logics

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,10 +1,44 @@
+"use client"
 
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
-function LeftSidebar(){
+function LeftSidebar() {
+
+    const router = useRouter();
+    const pathname = usePathname();
+
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
-                
+                {sidebarLinks.map((link) => {
+
+                    const isActive = 
+                        (pathname.includes(link.route) && link.route.length > 1) 
+                        || pathname === link.route;
+
+                    return (
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className="leftsidebar_link"
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+
+                            <p className="text-light-1 max-lg">
+                                {link.label}
+                            </p>
+                        </Link>
+                    )
+                }
+                )}
             </div>
         </section>
     )
