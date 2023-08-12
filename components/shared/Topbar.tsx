a2befc1fb59fc13a0737a8f09e0aca941ec0a940
valensixx <valensixx@gmail.com>
2023-08-12 13:16:33 +0300

    created clerk signup menu/button

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 
 function Topbar(){
     return (
@@ -16,10 +16,25 @@ function Topbar(){
                 <div className="block md:hidden">
                     <SignedIn>
                         <SignOutButton>
-
+                            <div className="flex cursor-pointer">
+                                <Image 
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24}
+                                />    
+                            </div>
                         </SignOutButton>
                     </SignedIn>
                 </div>
+
+                <OrganizationSwitcher 
+                    appearance={{
+                        elements: {
+                            organizationSwitcherTrigger:"py-2 px-4"
+                        }
+                    }}
+                />
             </div>
         </nav>
     )
