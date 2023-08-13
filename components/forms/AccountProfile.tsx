8f353c1a29aabe43d22ac07443d99d2eb6cebdd5
valensixx <valensixx@gmail.com>
2023-08-13 17:44:39 +0300

    workin on account profile

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,12 +1,20 @@
 "use client"
 
-import {useForm} from "react-hook-form";
-import {Form} from "@/components/ui/form";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { UserValidation } from "@/lib/validations/user";
+import * as z from "zod";
 
-interface Props{
-    user:{
+interface Props {
+    user: {
         id: string,
         objectId: string,
         username: string,
@@ -17,20 +25,41 @@ interface Props{
     btnTitle: string;
 }
 
-const AccountProfile = ({user, btnTitle}:Props) => {
+const AccountProfile = ({ user, btnTitle }: Props) => {
     const form = useForm({
         resolver: zodResolver(UserValidation),
-        defaultValues:{
+        defaultValues: {
             profile_photo: '',
             name: '',
             username: '',
             bio: ''
         }
     });
-    return (
-       <Form>
 
-       </Form>
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        //Do something with form values/
+        //This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        
+        <Form>
+            <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>This is your public display name.</FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+        </Form>
     )
 }
 
