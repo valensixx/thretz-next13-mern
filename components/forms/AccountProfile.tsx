f180197c4a3c3a74b5c8eb782ab766dc1ef59d6b
valensixx <valensixx@gmail.com>
2023-08-16 17:40:41 +0300

    start working on backend development

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -35,6 +35,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media");
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -67,13 +68,19 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
         const hasImageChanged = isBase64Image(blob);
 
         if (hasImageChanged) {
-            const imgRes =
+            const imgRes = await startUpload(files)
+
+            if(imgRes && imgRes[0].fileUrl){
+                values.profile_photo = imgRes[0].fileUrl;
+            }
         }
+
+        //todo: update user profile
     }
 
     return (
