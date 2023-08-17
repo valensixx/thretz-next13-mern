9df3d26b0f42e52a998c166dd44de91f0b38b9b7
valensixx <valensixx@gmail.com>
2023-08-17 12:13:13 +0300

    working on user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,22 @@
 "use server"
 
-import { connectToDB } from "../mongoose"
+import { connectToDB } from "../mongoose";
+import User from "../models/user.model";
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    path: string,
+    ): Promise<void> {
     connectToDB();
 
-    
+    await User.findOneAndUpdate(
+        { id: userId },
+        {
+            username: username.toLowerCase(),
+
+        }
+    )
 }
--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -4,12 +4,26 @@ const userSchema = new mongoose.Schema({
     id: { type: String, required: true },
     username: { type: String, required: true, unique:true },
     name: {type: String, required: true},
-    image: String,
+    image:  String,
     bio: String,
     threads:[
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Thread'
         }
+    ],
+    onboarded: {
+        type: Boolean,
+        default: false,
+    },
+    communities: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Community'
+        }
     ]
 });
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
