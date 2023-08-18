48ffc0aae5ea98b51d8eb603754e150a3076373e
valensixx <valensixx@gmail.com>
2023-08-18 17:55:47 +0300

    working on Postthread

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -10,7 +10,7 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
-//import { UserValidation } from "@/lib/validations/user";
+import { ThreadValidation } from "@/lib/validations/thread";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
@@ -29,19 +29,52 @@ interface Props {
     btnTitle: string;
 }
 
-function PostThread( {userId}: {userId: string}){
+function PostThread({ userId }: { userId: string }) {
     const router = useRouter();
     const pathname = usePathname();
 
     const form = useForm({
-        resolver: zodResolver(),
+        resolver: zodResolver(ThreadValidation),
         defaultValues: {
-           thread: '',
-           accountId: userId,
-           
+            thread: '',
+            accountId: userId,
+
         }
-    });
-   return <h1>Post Thread Form</h1>
+    })
+
+    const onSubmit = () => {
+
+    }
+
+    return (
+        <Form {...form}>
+            <form
+                className='mt-10 flex flex-col justify-start gap-10'
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <FormField
+                    control={form.control}
+                    name='thread'
+                    render={({ field }) => (
+                        <FormItem className='flex w-full flex-col gap-3'>
+                            <FormLabel className='text-base-semibold text-light-2'>
+                                Content
+                            </FormLabel>
+                            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                                <Textarea
+                                    rows={15}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="bg-primary-500">
+                    Post Thread
+                </Button>
+            </form>
+        </Form>
+    )
 }
 
 export default PostThread;
