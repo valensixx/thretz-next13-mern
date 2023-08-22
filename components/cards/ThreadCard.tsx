2136330e8c6b2767eb80e0f9211673e15314073c
valensixx <valensixx@gmail.com>
2023-08-22 14:18:05 +0300

    component done for now

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
     community,
     createdAt,
     comments,
+    isComment,
 }: Props) => {
     return(
         <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
@@ -51,17 +52,41 @@ const ThreadCard = ({
 
                         <div className="thread-card_bar" />  
                     </div>
-                    
-                    <div>
-                        <Link href={`/profile`}>
+
+                    <div className="flex w-full flex-col">
+                        <Link href={`/profile/$author.id`} className="w-fit">
+                            <h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4>
                         </Link>
+
+                        <p className="mt-2 text-small-regular text-light-2">{content}</p>
+
+                        <div className="mt-5 flex flex-col gap-3">
+                            <div className="flex gap-3.5">
+                                <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} 
+                                className="cursor-pointer object-contain"
+                                />
+                                <Link href={`/thread/${id}`}>
+                                    <Image src="/assets/reply.svg" alt="reply" width={24} height={24} 
+                                    className="cursor-pointer object-contain"
+                                    />
+                                </Link>
+                                <Image src="/assets/repost.svg" alt="repost" width={24} height={24} 
+                                className="cursor-pointer object-contain"
+                                />
+                                <Image src="/assets/share.svg" alt="share" width={24} height={24} 
+                                className="cursor-pointer object-contain"
+                                />
+                            </div>
+
+                            {isComment && comments.length > 0 && (
+                                <Link href={`/thread/${id}`}>
+                                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                                </Link>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <h2 className="text-small-regular text-light-2">
-                {content}
-            </h2>
         </article>
     )
 }
