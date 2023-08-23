c77064a332c2e3ee3b6e86b2df27f9221ad749d4
valensixx <valensixx@gmail.com>
2023-08-23 15:42:44 +0300

    working on comment component

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -16,6 +16,7 @@ import { Button } from "@/components/ui/button";
 import { Input} from "@/components/ui/input";
 import { updateUser } from "@/lib/actions/user.actions";
 import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
 //import { createThread } from "@/lib/actions/thread.actions";
 
 interface Props {
@@ -48,31 +49,36 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
     return (
         <Form {...form}>
             <form
-                className='mt-10 flex flex-col justify-start gap-10'
+                className='comment-form'
                 onSubmit={form.handleSubmit(onSubmit)}
             >
                 <FormField
                     control={form.control}
                     name='thread'
                     render={({ field }) => (
-                        <FormItem className='flex w-full flex-col gap-3'>
-                            <FormLabel className='text-base-semibold text-light-2'>
-                                Content
+                        <FormItem className='flex w-full items-center gap-3'>
+                            <FormLabel>
+                                <Image 
+                                    src={currentUserImg}
+                                    alt="profile picture"
+                                    width={48}
+                                    height={48}
+                                    className="rounded-full object-cover"
+                                />
                             </FormLabel>
-                            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                            <FormControl className="border-none bg-transperant">
                                 <Input
                                     type="text"
                                     placeholder="Comment..."
-                                    className="on-focus text-light-1 outline-none "
+                                    className="on-focus text-light-1 outline-none"
                                     {...field}
                                 />
                             </FormControl>
-                            <FormMessage />
                         </FormItem>
                     )}
                 />
-                <Button type="submit" className="bg-primary-500">
-                    Post Thread
+                <Button type="submit" className="comment-form_btn">
+                    Reply
                 </Button>
             </form>
         </Form>
