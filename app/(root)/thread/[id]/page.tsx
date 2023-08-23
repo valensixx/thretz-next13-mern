e4cad72dd6ce0439a81b3a13e92e758022d0f976
valensixx <valensixx@gmail.com>
2023-08-23 17:53:38 +0300

    working on profile page

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -7,40 +7,57 @@ import { redirect } from "next/navigation";
 import { stringify } from "querystring";
 
 const Page = async ({ params }: { params: { id: string } }) => {
-    if(!params.id) return null;
+    if (!params.id) return null;
 
     const user = await currentUser();
-    if(!user) return null;
+    if (!user) return null;
 
     const userInfo = await fetchUser(user.id);
-    if(!userInfo?.onboarded) redirect('/onboarding');
+    if (!userInfo?.onboarded) redirect('/onboarding');
 
     const thread = await fetchThreadById(params.id)
 
-    return(
-    <section className="relative">
-        <div>
-            <ThreadCard
-                key={thread._id}
-                id={thread._id}
-                currentUserId={user?.id || ""}
-                parentId={thread.parentId}
-                content={thread.text}
-                author={thread.author}
-                community={thread.community}
-                createdAt={thread.createAt}
-                comments={thread.children}
-            />
-        </div>
+    return (
+        <section className="relative">
+            <div>
+                <ThreadCard
+                    key={thread._id}
+                    id={thread._id}
+                    currentUserId={user?.id || ""}
+                    parentId={thread.parentId}
+                    content={thread.text}
+                    author={thread.author}
+                    community={thread.community}
+                    createdAt={thread.createAt}
+                    comments={thread.children}
+                />
+            </div>
 
-        <div className="mt-7">
-            <Comment
-                threadId={thread.id}
-                currentUserImg={user.imageUrl}
-                currentUserId={JSON.stringify(userInfo._id)}
-            />
-        </div>
-    </section>
+            <div className="mt-7">
+                <Comment
+                    threadId={thread.id}
+                    currentUserImg={user.imageUrl}
+                    currentUserId={JSON.stringify(userInfo._id)}
+                />
+            </div>
+
+            <div className="mt-10">
+                {thread.children.map((childItem: any) => (
+                    <ThreadCard
+                        key={childItem._id}
+                        id={childItem._id}
+                        currentUserId={childItem?.id || ""}
+                        parentId={childItem.parentId}
+                        content={childItem.text}
+                        author={childItem.author}
+                        community={childItem.community}
+                        createdAt={childItem.createAt}
+                        comments={childItem.children}
+                        isComment
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
 
