3fdb67e7d0251b28b2cf080375927eb5e2e1376a
valensixx <valensixx@gmail.com>
2023-08-24 11:44:26 +0300

    working on Profile Header

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,9 +1,24 @@
-const Page = async () => {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+
+
+async function Page({params}:{params:{id: string}}){
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(params.id);
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
     return (
         <section>
-            Profile
+            <ProfileHeader 
+
+            />
         </section>
     )
 }
 
-export default Page();
+export default Page;
---
+++ components/shared/ProfileHeader.tsx
@@ -0,0 +1,11 @@
+const ProfileHeader = () => {
+    return(
+        <div>
+            <h1>
+                Profile Header
+            </h1>
+        </div>
+    )
+}
+
+export default ProfileHeader;
