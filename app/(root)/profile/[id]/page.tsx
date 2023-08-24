29b3d68185f0dabb5ca586be1a2d2fea0a132a62
valensixx <valensixx@gmail.com>
2023-08-24 12:56:03 +0300

    ThreadsTab component

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { fetchUser } from "@/lib/actions/user.actions";
 import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsList } from "@radix-ui/react-tabs";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
 
 
 async function Page({params}:{params:{id: string}}){
@@ -25,10 +30,36 @@ async function Page({params}:{params:{id: string}}){
             />
 
             <div className="mt-9">
-                <Tabs>
-                    <TabsList>
-                        
+                <Tabs defaultValue="threads" className="w-full">
+                    <TabsList className="tab">
+                        {profileTabs.map((tab)=>(
+                            <TabsTrigger key={tab.label} value={tab.value} className="tab"> 
+                                <Image 
+                                    src={tab.icon}
+                                    alt={tab.icon}
+                                    height={24}
+                                    width={24}
+                                    className="object-contain"
+                                />
+                                <p className="max-sm:hidden">{tab.label}</p>
+
+                                {tab.label === 'Therads' && (
+                                    <p className="ml-1 rounded-sm bg-light-4 px-2 py-2 !text-tiny-medium text-light-2">
+                                        {userInfo?.threads?.length}
+                                    </p>
+                                )}
+                            </TabsTrigger>
+                        ))}
                     </TabsList>
+                    {profileTabs.map((tab)=>(
+                        <TabsContent key={`content-${tab.label}`} value={tab.value} className="w-full text-light-1">
+                            <ThreadsTab 
+                                currentUserId={user.id}
+                                accountId={userInfo.id}
+                                accountType="User"
+                            />
+                        </TabsContent>
+                    ))}
                 </Tabs>
             </div>
         </section>
