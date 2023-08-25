b5b824cacec8f6796e712553a53fbf563f753c84
valensixx <valensixx@gmail.com>
2023-08-25 13:40:55 +0300

    working on activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,21 @@
-const Page = async () => {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+
+
+
+async function Page(){
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    //getActivity 
+
     return (
       <section>
           <h1 className="head-text mb-10">
